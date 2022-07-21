e258489cb0e356267ed3b263652c341aec583f55
windi <windi.azmy@gmail.com>
2022-07-21 20:44:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+//import { Route, Routes, Link } from 'react-router-dom';
+import React from 'react';
+
+import Navbar from './components/Navbar';
+//import Footer from './components/Footer';
+import { ThemeProvider } from "@mui/material";
+import theme from "./themes/theme";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar></Navbar>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/theme.js
@@ -0,0 +1,17 @@
+import {createTheme} from '@mui/material/styles';
+
+const theme = createTheme({
+     type: 'light',
+     primary: {
+      main: '#141414',
+      contrastText: '#ffffff',
+      light: 'rgba(109,109,100,0.7)',
+    },
+    secondary: {
+      main: '#e2d7da',
+    },
+})
+
+
+
+export default theme;
